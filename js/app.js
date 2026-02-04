85f649b7047b9c4e9e45b5883674c5745ead3962
outeglaout jaouad <outeglaoutjaouad@gmail.com>
2026-02-04 17:00:37 +0100

    Creating toggle screens feature

---
+++ js/app.js
@@ -0,0 +1,26 @@
+// Toogle between screens & tab : the first one has activated.
+
+// Tab doc:
+const tabs = document.querySelector(".tabs");
+const screens = document.querySelector(".screens");
+let currentActiveTab = tabs.children[0];
+let currentActiveScreen = screens.children[0];
+
+tabs.addEventListener("click", (e) => {
+  let target = e.target.closest(".tab");
+  if (!target) return null;
+  let targetedTab = target.classList[1];
+
+  // Remove the activated class from the 'currentActiveTab' & 'currentActiveScreen';
+  currentActiveTab.classList.remove("activated");
+  currentActiveScreen.classList.remove("activated");
+
+  // Add the activated class to the 'targeted Tab' & 'targeted Screen';
+  let targetScreen = screens.children.item(+targetedTab);
+  target.classList.add("activated");
+  targetScreen.classList.add("activated");
+
+  // Assign
+  currentActiveTab = target;
+  currentActiveScreen = targetScreen;
+});
