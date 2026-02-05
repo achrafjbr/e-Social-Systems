dedc913b5d4b1e0afa4b42df584d5ea5bcce613a
outeglaout jaouad <outeglaoutjaouad@gmail.com>
2026-02-05 12:12:35 +0100

    Adding Employeur, and Declaration classes and data file where we gonna manipulate with data

---
+++ js/data.js
@@ -0,0 +1,149 @@
+
+// Example Data project :
+
+data = [
+// employeur 1
+  {
+    id: "",
+    raisonSocial: "",
+    secteur: "",
+    declaration: [
+      {
+        id: "",
+        anneeAndmois: "",
+        dateDeclaration: "",
+        enRetard: "",
+        penalité: "",
+      },
+      {
+        id: "",
+        anneeAndmois: "",
+        dateDeclaration: "",
+        enRetard: "",
+        penalité: "",
+      },
+    ],
+    employees: [
+      {
+        id: "",
+        fullName: "",
+        salary: "",
+        cotisation: [
+          {
+            id: "",
+            tauxCotisation: "",
+            date: "",
+          },
+          {
+            id: "",
+            tauxCotisation: "",
+            date: "",
+          },
+        ],
+      },
+
+      {
+        id: "",
+        fullName: "",
+        salary: "",
+        cotisation: [
+          {
+            id: "",
+            tauxCotisation: "",
+            date: "",
+          },
+          {
+            id: "",
+            tauxCotisation: "",
+            date: "",
+          },
+        ],
+      },
+    ],
+    cotisation: [
+      {
+        id: "",
+        tauxCotisation: "",
+        date: "",
+      },
+      {
+        id: "",
+        tauxCotisation: "",
+        date: "",
+      },
+    ],
+  },
+
+
+// employeur 2
+   {
+    id: "",
+    raisonSocial: "",
+    secteur: "",
+    declaration: [
+      {
+        id: "",
+        anneeAndmois: "",
+        dateDeclaration: "",
+        enRetard: "",
+        penalité: "",
+      },
+      {
+        id: "",
+        anneeAndmois: "",
+        dateDeclaration: "",
+        enRetard: "",
+        penalité: "",
+      },
+    ],
+    employees: [
+      {
+        id: "",
+        fullName: "",
+        salary: "",
+        cotisation: [
+          {
+            id: "",
+            tauxCotisation: "",
+            date: "",
+          },
+          {
+            id: "",
+            tauxCotisation: "",
+            date: "",
+          },
+        ],
+      },
+
+      {
+        id: "",
+        fullName: "",
+        salary: "",
+        cotisation: [
+          {
+            id: "",
+            tauxCotisation: "",
+            date: "",
+          },
+          {
+            id: "",
+            tauxCotisation: "",
+            date: "",
+          },
+        ],
+      },
+    ],
+    cotisation: [
+      {
+        id: "",
+        tauxCotisation: "",
+        date: "",
+      },
+      {
+        id: "",
+        tauxCotisation: "",
+        date: "",
+      },
+    ],
+  },
+];
