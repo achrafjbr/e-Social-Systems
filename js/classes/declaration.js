09429f3f38881365a941b711a816acf78f18d10d
outeglaout jaouad <outeglaoutjaouad@gmail.com>
2026-02-06 17:07:21 +0100

    Handling the logic of declaration project phase

--- js/classes/declaration.js
+++ js/classes/declaration.js
@@ -22,18 +22,72 @@ const declarationDate = document.getElementById("declaration-date");
 
 const saveEmployeurBtn = document.querySelector(".save_employeur");
 
+const joinEmployeurWithDeclarationForRerenderingTable = () => {
+  employeurs.forEach((employeur) => {
+    declarations.forEach((declaration) => {
+       let declarationEmployeur = declaration.employeurId == employeur.id;
+
+       reRenderDeclarationTable(declarationEmployeur, employeur);
+    });
+  });
+};
+const reRenderDeclarationTable = (declarationEmployeur, employeur) => {
+
+  let table = `<tr>
+                <td>${employeur.sociale}</td>
+                <td>${declarationEmployeur.anneeMois}</td>
+                <td>${declarationEmployeur.dateDeclaration}</td>
+                <td class="isAjour">À jour</td>
+                <td>1260 DH</td>
+              </tr>`;
+
+        document.querySelector('.employeur_table_data')
+        .appendChild(table);
+};
+
 const addDeclaration = () => {
-  console.log(socialSelection, anneeMois.value, declarationDate.value);
-  // convert input date to js date
-  const date = new Date(anneeMois.value);
-  const decDate = new Date(declarationDate.value);
+  // console.log(socialSelection, anneeMois.value, declarationDate.value);
+
   // Get employeur which has this ' employeurSelection '
-  console.log(findEmployeurBySociale(socialSelection));
-  // then add declaration of this employeur
+  const employeurId = findEmployeurBySociale(socialSelection).id;
+
+  // Calculate the penalité of this employeur.
+  const penalityRate = calculateThePenalityOfLating(
+    anneeMois.value,
+    declarationDate.value,
+  );
+
+  // declaration Object
+  let declaration = {
+    id: Date.now(),
+    anneeMois: anneeMois.value,
+    dateDeclaration: declarationDate.value,
+    penalité: penalityRate,
+    employeurId: employeurId,
+  };
+
+  // Then add declaration of this employeur
+  declarations.push(declaration);
+
+  // reRender table screen.
+  joinEmployeurWithDeclarationForRerenderingTable();
 };
 
 saveEmployeurBtn.addEventListener("click", addDeclaration);
 
 const findEmployeurBySociale = (sociale) => {
-  employeurs.filter((employeur)=> employeur.sociale == sociale);
+  for (const employeur of employeurs) {
+    if (employeur.sociale == sociale) {
+      return employeur;
+    }
+  }
+};
+
+const calculateThePenalityOfLating = (date, decDate) => {
+  // Convert input date to js date.
+  const date = new Date(date);
+  const decDate = new Date(decDate);
+  const payDay = date.getDate();
+  const decDay = decDate.getDate();
+  return (payDay - decDay) * 0.01;
 };
