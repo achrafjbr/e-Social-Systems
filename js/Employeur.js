161604f8bf8dd0bb20b416e7180c5e8998221c5b
Hibaboudiaj <boudiajhiba2@gmail.com>
2026-02-06 15:17:40 +0100

    Function Afficher Et supprimer Terminé

--- js/Employeur.js
+++ js/Employeur.js
@@ -6,6 +6,8 @@ const Secteur = document.querySelector("#Secteur");
 const form = document.querySelector(".left form");
 const p = document.querySelector("#message");
 
+const tablebody = document.querySelector("#tableauBody");
+//---function ajouter start---//
 form.addEventListener("submit", function (e) {
   e.preventDefault();
 
@@ -21,6 +23,45 @@ form.addEventListener("submit", function (e) {
     myarray.push(chek);
     p.textContent = "Employeur ajouté avec succès";
   }
-
+  Afficher();
   form.reset();
 });
+//---function ajouter ends---//
+
+//---function afficher start---//
+function Afficher() {
+  tablebody.innerHTML = "";
+  if (myarray.length == 0) {
+    tablebody.innerHTML = `
+      <tr>
+        <td colspan="5" style="text-align:center; font-weight: 600">
+          Aucun Rendez-Vous Ajouté Pour L'instant
+        </td>
+      </tr>
+    `;
+  } else {
+    myarray.forEach((elm, index) => {
+      let tr = document.createElement("tr");
+      tr.innerHTML = `
+      <td>${elm.employeur}</td>
+      <td>${elm.sociale}</td>
+      <td>${elm.secteur}</td>
+      <td>${elm.secteur}</td>
+      <td><span class="material-symbols-outlined deleteBtn"
+      onclick = "deleteItem(${index})">
+    delete
+    </span></td>
+      `;
+      tablebody.appendChild(tr);
+    });
+  }
+}
+//---function afficher ends---//
+
+//---Function delete start---//
+function deleteItem(index) {
+  myarray.splice(index, 1);
+  Afficher();
+}
+//---Function delete ends---//
+Afficher();
