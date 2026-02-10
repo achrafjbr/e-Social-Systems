cd89e94689cdbac9bbc9087062cd18fb9e81f033
AbderrahmanNaciri22 <abderrhmanen2205@gmail.com>
2026-02-10 09:37:47 +0100

    test

---
+++ js/statistiques.js
@@ -0,0 +1,8 @@
+import { employeurCotisation ,Sector,employeur,employee,declaration } from "./data.js";
+
+const EmpEnergCount = document.getElementById("EmpEnergCount");
+
+employeur.forEach(employeur)
+
+EmpEnergCount.textContent = "10";
+
