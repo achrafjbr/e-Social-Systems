d74945e908287d82776c38a5d4eea2760de33c96
Rida Taki <??????? ????? ?? GitHub>
2026-02-10 17:16:29 +0100

    filtrage and storge

--- css/employee.js
+++ css/employee.js
@@ -1,6 +1,19 @@
 const Ajouter = document.getElementById("Ajouter");
 const tbody = document.getElementById("tbody");
 
+let stock = JSON.parse(localStorage.getItem("stock")) || [];
+
+// 🔹 نعرضو data القديمة
+stock.forEach(emp => {
+  const tr = document.createElement("tr");
+  tr.innerHTML = `
+    <td>${emp.Nom}</td>
+    <td>${emp.Salaire}</td>
+    <td>${emp.Employer}</td>
+  `;
+  tbody.appendChild(tr);
+});
+
 Ajouter.addEventListener("click" , function(e){
     e.preventDefault();
    
@@ -20,6 +33,18 @@ if(isNaN(Salaire) || Salaire<=0){
 return;
 }
 
+  // 🔹 1. نصايبو object
+  const employee = {
+    Nom: Nom,
+    Salaire: Salaire,
+    Employer: Employer
+  };
+
+  // 🔹 2. نضيفوه للـ array
+  stock.push(employee);
+
+  // 🔹 3. نخزّنو ف localStorage
+  localStorage.setItem("stock", JSON.stringify(stock))
 
     const tr = document.createElement("tr");
     tr.innerHTML =`
@@ -29,4 +54,30 @@ return;
     `;
 
     tbody.appendChild(tr);
+
 });
+const searchIcon = document.querySelector(".search-icon");
+const searchInput = document.querySelector(".insearch");
+
+searchIcon.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  const searchValue = searchInput.value.toLowerCase();
+  const rows = document.querySelectorAll("#tbody tr");
+
+  rows.forEach((tr) => {
+    const nomTd = tr.children[0].textContent.toLowerCase();
+
+    // نرجعو اللون عادي
+    tr.style.backgroundColor = "";
+
+    if (nomTd === searchValue && searchValue !== "") {
+      tr.style.backgroundColor = "lightgreen";
+    }
+  });
+});
+
+
+// insearch= input 
+// search-icon = icon 
+// serchelement = div li jma3 les element 2
